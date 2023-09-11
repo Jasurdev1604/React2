e8c49181d33fdc1d5084f28e5ca638f68607d6ed
jasurbek <jasurdev1604@gmail.com>
2023-09-11 14:17:46 +0500

    props, components, export-import, childres

--- src/lesson2/index.jsx
+++ src/lesson2/index.jsx
@@ -2,15 +2,18 @@ import React, { Components } from "react";
 
 class Index extends React.Component {
   render() {
-    const { title, status } = this.props;
+    // const { title, status } = this.props;
 
     return (
       <React.StrictMode>
-        <h1>
-          Jasurbek {title} {status || "none"}
+        {/* <h1> */}
+        {/* Jasurbek {title} {status || "none"}
         </h1>
-        <p>{this.props?.data?.name}</p>
-        <h2>{this.props.children}</h2>
+        <p>{this.props?.data?.name}</p> */}
+        {/* <h2>{this.props.children}</h2> */}
+        <h1>
+          {this.props.data.id} - {this.props.data.name} - {this.props.data.age}
+        </h1>
       </React.StrictMode>
     );
   }
--- src/lesson2/root.jsx
+++ src/lesson2/root.jsx
@@ -1,13 +1,34 @@
 import React from "react";
 import Index from "./index";
 
+const Students = [
+  {
+    id: 1,
+    name: "jasurbek",
+    age: 19,
+  },
+  {
+    id: 2,
+    name: "jasurbek",
+    age: 19,
+  },
+  {
+    id: 3,
+    name: "jasurbek",
+    age: 19,
+  },
+];
+
 export const root = () => {
   return (
     <React.StrictMode>
-      <Index status="active" title="react.js" />
+      {/* <Index status="active" title="react.js" />
       <Index title="vue.js" />
       <Index title="angular.js" />
-      <Index data={{ name: "jasurbek", age: 19 }}>children</Index>
+      <Index data={{ name: "jasurbek", age: 19 }}>children</Index> */}
+      {Students.map((e) => {
+        return <Index key={e.id} data={e} />;
+      })}
     </React.StrictMode>
   );
 };
