faeabcbec206bdfcdbf90586861336652b8786bf
jasurbek <jasurdev1604@gmail.com>
2023-09-14 13:12:11 +0500

    map, key, render

--- src/lesson8/index.jsx
+++ src/lesson8/index.jsx
@@ -1,24 +1,46 @@
 import React, { Component } from "react";
 import SignIn from "./components/SignIn";
 import SignUp from "./components/SignUp";
+
+const list = [
+  { id: 1, name: "jasurbek" },
+  { id: 2, name: "eshmat" },
+  { id: 3, name: "toshmat" },
+  { id: 4, name: "gulmat" },
+];
+
 export default class index extends Component {
   constructor(props) {
     super(props);
     this.state = {
       login: false,
+      data: list,
     };
   }
+
   render() {
-    const getRes = (value) => {
-      this.setState({ login: value });
+    // const getRes = (value) => {
+    //   this.setState({ login: value });
+    // };
+    const del = (id) => {
+      let res = this.state.data.filter((v) => v.id !== id);
+      this.setState({ data, res });
     };
     return (
       <React.StrictMode>
-        {this.state.login ? (
+        {/* {this.state.login ? (
           <SignIn getRes={getRes} />
         ) : (
           <SignUp getRes={getRes} />
-        )}
+        )} */}
+        <h1>List</h1>
+        {this.state.data.map(({ id, name }) => {
+          return (
+            <h1>
+              {id} {name} <button onClick={() => del(id)}>del</button>
+            </h1>
+          );
+        })}
       </React.StrictMode>
     );
   }
--- src/lesson8/review.js
+++ src/lesson8/review.js
@@ -7,3 +7,5 @@
 // 	- 1 => 2ta components => signin, signup
 // 	- 2 => UI
 // 	- 3 => functional buttons
+
+//! key = > datami map qilganda orab turuvchi containerga key = {unique_id} beriladi -> state ozgarganda hamma malumotni qayta chinishni oldini oladi.
