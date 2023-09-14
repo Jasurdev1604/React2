30c15ebcfc3bb8249cf8d4896aa399df1282b7b8
jasurbek <jasurdev1604@gmail.com>
2023-09-14 14:19:47 +0500

    React Lifecycles & Binding

--- src/lesson9/index.jsx
+++ src/lesson9/index.jsx
@@ -1,10 +1,67 @@
 import React, { Component } from "react";
 
 export default class index extends Component {
+  constructor(props) {
+    super(props);
+    console.log("constructor");
+    this.state = {
+      count: 0,
+    };
+  }
+
+  componentWillMount() {
+    //birinchi rendrden oldin yuradi
+    console.log("this.componentWillMount");
+  }
+
+  componentDidMount() {
+    //birinchi renderdan keyin
+    console.log("this.componentDidMount");
+  }
+
+  componentWillReceiveProps() {
+    //child parnetdan prop olgan payda yuradi
+    console.log("this.componentWillReceiveProps");
+  }
+
+  shouldComponentUpdate() {
+    //componentni updat quilish yoki qilmaslilni
+    console.log("this.shouldComponentUpdate");
+    return true;
+  }
+
+  componentWillUpdate() {
+    //renderdan oldin va prop kelishidan keyin
+    console.log("this.componentWillUpdate");
+  }
+
+  componentDidUpdate() {
+    //component re render bolgandan ketin ishlaydi
+    console.log("this.componentDidUpdate");
+  }
+
+  componentWillUnmount() {
+    //DOM elementi ozgarishida keyin
+    console.log(this.componentWillUnmount);
+  }
+
+  plus() {
+    this.setState({ count: this.state.count + 1 });
+  }
+
+  minus() {
+    this.setState({ count: this.state.count - 1 });
+  }
+
   render() {
+    console.log("render");
+
     return (
       <React.StrictMode>
-        <h1>Index</h1>
+        <h1>React Lifecycles and Binding</h1>
+        <h2>Count:{this.state.count}</h2>
+        <button onClick={this.plus.bind(this)}>plus</button>
+        <button onClick={() => this.minus()}>minus</button>
       </React.StrictMode>
     );
   }
--- src/lesson9/review.js
+++ src/lesson9/review.js
@@ -1,3 +1,9 @@
 //! 9-dars
 // 	- ReactJS => 7 Lifecycles
 // 	- bind => 3 ways
+
+//!React Lyfecycles => hozirgi kunda ishlatilmaydi unga alternative hooks bor , vazifasi biror actiondankeyin or oldin yoziladigan eventlar
+
+//!Binding -> class componentlarda boladi ,
+
+//class componentda functionlar render(){} dan oldin yozilganda this yoqolip qiadi va shu functionni chqaurganimizda bind(this) qilishimizkerak
