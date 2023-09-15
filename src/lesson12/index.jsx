8c4c00ec891ee2de9848095d505949ed9e9d4d5e
jasurbek <jasurdev1604@gmail.com>
2023-09-15 17:34:29 +0500

    useReducer

--- src/lesson12/index.jsx
+++ src/lesson12/index.jsx
@@ -1,4 +1,4 @@
-import React, { useReducer } from "react";
+import React, { useReducer, useState } from "react";
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -20,6 +20,12 @@ const initialState = 0;
 const Index = () => {
   const [counter, dispatch] = useReducer(reducer, initialState);
 
+  const [btn, setBtn] = useState(1);
+
+  const onSelect = ({ target: { value } }) => {
+    setBtn(value);
+  };
+
   return (
     <React.Fragment>
       <h1>useReducer</h1>
@@ -29,8 +35,17 @@ const Index = () => {
       <button onClick={() => dispatch({ type: "bymount", payload: 20 })}>
         20
       </button>
-      <button onClick={() => dispatch({ type: "bymount", payload: 100 })}>
-        100
+      <select onChange={onSelect}>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+      </select>
+      <button
+        onClick={() => dispatch({ type: "bymount", payload: Number(btn) })}
+      >
+        {btn} ga orttirish
       </button>
     </React.Fragment>
   );
