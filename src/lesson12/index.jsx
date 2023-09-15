0323b6c07fbfe61aa6eeab1b263b782ce5897b4d
jasurbek <jasurdev1604@gmail.com>
2023-09-15 17:27:48 +0500

    useReducer

--- src/lesson12/index.jsx
+++ src/lesson12/index.jsx
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useReducer } from "react";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "plus":
+      return state + 1;
+      break;
+    case "minus":
+      return state - 1;
+    case "bymount":
+      return state + action.payload;
+    default:
+      return state;
+      break;
+  }
+};
+
+const initialState = 0;
 
 const Index = () => {
-  return <div>index</div>;
+  const [counter, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <React.Fragment>
+      <h1>useReducer</h1>
+      <h1>Counter :{counter}</h1>
+      <button onClick={() => dispatch({ type: "plus" })}>plus</button>
+      <button onClick={() => dispatch({ type: "minus" })}>minus</button>
+      <button onClick={() => dispatch({ type: "bymount", payload: 20 })}>
+        20
+      </button>
+      <button onClick={() => dispatch({ type: "bymount", payload: 100 })}>
+        100
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default Index;
--- src/lesson12/review.js
+++ src/lesson12/review.js
@@ -1,2 +1,6 @@
 //! 12-dars
-// 	- useReducer
+// 	- useReducer -> contextAPI bilan birgalikda redux ni ornini bosadi
+
+import { useReducer } from "react";
+
+// const [counter , dispatch] = useReducer(callback(count, state) , initialState)
