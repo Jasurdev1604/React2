91c4a8bb407ee015ca96f5a49ed8358293ba40df
jasurbek <jasurdev1604@gmail.com>
2023-09-15 17:40:08 +0500

    useReducer

--- src/lesson12/review.js
+++ src/lesson12/review.js
@@ -4,3 +4,29 @@
 import { useReducer } from "react";
 
 // const [counter , dispatch] = useReducer(callback(count, state) , initialState)
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "plus":
+      return state + 1;
+      break;
+    case "minus":
+      return state - 1;
+    case "bymount":
+      return state + action.payload;
+    default:
+      return state;
+      break;
+  }
+};
+
+const initialState = 0;
+
+const [counter, dispatch] = useReducer(reducer, initialState);
+
+{
+  /* <h1>Counter :{counter}</h1>
+<button onClick={() => dispatch({ type: "plus" })}>plus</button>
+<button onClick={() => dispatch({ type: "minus" })}>minus</button>
+<button onClick={() => dispatch({ type: "bymount", payload: 20 })}> */
+}
