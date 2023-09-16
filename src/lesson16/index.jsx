2c92669987e15cbc349b0804f2dfc794a471598b
jasurbek <jasurdev1604@gmail.com>
2023-09-16 18:21:05 +0500

    LocalStorage

--- src/lesson16/index.jsx
+++ src/lesson16/index.jsx
@@ -1,5 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function index() {
-  return <React.Fragment>index</React.Fragment>;
+  const [count, setCount] = useState(
+    JSON.parse(localStorage.getItem("count")) || { data: 0 }
+  );
+  const plus = () => {
+    setCount({ data: count.data + 1 });
+    localStorage.setItem("count", JSON.stringify({ data: count.data + 1 }));
+  };
+  const minus = () => {
+    setCount({ data: count.data - 1 });
+    localStorage.setItem("count", JSON.stringify({ data: count.data - 1 }));
+  };
+  const clear = () => {
+    localStorage.setItem("count", JSON.stringify({ data: 0 }));
+    setCount({ data: 0 });
+  };
+  return (
+    <React.Fragment>
+      <h1>LocalStorage</h1>
+      <h1>Count:{count.data}</h1>
+      <button onClick={plus}>plus</button>
+      <button onClick={minus}>minus</button>
+      <button onClick={clear}>clear</button>
+    </React.Fragment>
+  );
 }
