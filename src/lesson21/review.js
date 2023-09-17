291e223667c78149e840643d37484f3440859387
jasurbek <jasurdev1604@gmail.com>
2023-09-17 21:58:57 +0500

    React Router DOM 5

---
+++ src/lesson21/review.js
@@ -0,0 +1,10 @@
+//! 21-dars
+// 	- React Router DOM 5
+// 	- BrowserRouter
+// 	- Navlink, Link
+// 	- Route
+// 	- useHistory, useLocation => window.location
+// 	- Switch
+// 	- usePrams
+// 	- privateRoute
+// 	- redirect
