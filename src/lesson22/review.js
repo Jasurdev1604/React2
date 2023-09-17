4eb29a27a1ba08e6fec058f53e1c602f3d35e93c
jasurbek <jasurdev1604@gmail.com>
2023-09-17 22:00:04 +0500

    React Router DOM 6

---
+++ src/lesson22/review.js
@@ -0,0 +1,11 @@
+//! 22-dars
+// 	- React Router DOM 6
+// 	- redirect -> navigate
+// 	- useHistory -> useNavigate
+// 	- Switch -> Routes
+// 	- Route -> component -> element
+// 	- Outlet
+// 	- Navlink activeStyle -> style={({isActive})=>}
+// 	- usePrams
+// 	- privateRoute
+// 	- optimal navbar yasash
