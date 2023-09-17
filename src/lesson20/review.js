2fda5123a38f0016cf4d6d7979d7794039b08bc2
jasurbek <jasurdev1604@gmail.com>
2023-09-17 21:57:26 +0500

    axios

---
+++ src/lesson20/review.js
@@ -0,0 +1,6 @@
+//! 20-dars
+// 	- axios
+// 	- fetch vs axios
+// 	- vanilla js vs 3rd part library
+// 	- manually vs automatic => data transform
+// 	- npm- install whatwg-fetch --save polyfill for-fetch
