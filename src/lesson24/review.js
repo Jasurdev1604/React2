6d28c102df81e79454024e696083598222fdd814
jasurbek <jasurdev1604@gmail.com>
2023-09-17 22:02:17 +0500

    React 18V

---
+++ src/lesson24/review.js
@@ -0,0 +1,10 @@
+//! 24-dars
+// 	- React 18V
+// 	- batching
+// 	- startTransition
+// 	- New Suspense
+// 	- useId()
+// 	- useTransition()
+// 	- useDeferredValue()
+// 	- useSyncExternalStore()
+// 	- useInsertionEffect()
