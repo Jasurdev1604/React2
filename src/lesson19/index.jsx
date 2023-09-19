fd90ffe115d136f91fe66ac6912fb9f1a61e3fd1
jasurbek <jasurdev1604@gmail.com>
2023-09-19 21:23:36 +0500

    restful api

--- src/lesson19/index.jsx
+++ src/lesson19/index.jsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function index() {
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((res) => setStudents(res));
+  }, []);
+
+  const [students, setStudents] = useState([]);
   return (
     <React.Fragment>
       <h1>Restful API</h1>
+      <h1>Students List</h1>
+      <ul>
+        {students.map(({ id, name }) => (
+          <li key={id}>
+            {id} - {name}
+          </li>
+        ))}
+      </ul>
     </React.Fragment>
   );
 }
