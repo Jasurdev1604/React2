5070eb689bec616a11ffd801d511cef6b9fd6dad
jasurbek <jasurdev1604@gmail.com>
2023-09-20 13:39:13 +0500

    restful api

--- src/lesson19/Login.jsx
+++ src/lesson19/Login.jsx
@@ -1,41 +1,42 @@
 import React, { useRef } from "react";
 
 export default function Crud() {
-  const emailRef = useRef();
-  const pwRef = useRef();
-  const onLogin = () => {
-    fetch("http://localhost:8080/api/public/auth/login", {
+  const getUsers = () => {
+    return fetch(`http://localhost:8080/api/public/auth/login`, {
       method: "POST",
       headers: {
-        Authorization: `Bearer ${localStorage.getItem("token")}`,
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        email: emailRef?.current?.value,
-        password: pwRef?.current?.value,
+        email: emailRef.current.value,
+        password: passwordRef.current.value,
       }),
     })
       .then((res) => res.json())
-      .then((res) => localStorage.setItem("token", res.authenticationToken));
+      .then((res) => {
+        localStorage.setItem("token", res.authenticationToken);
+      });
   };
+
+  const emailRef = useRef("");
+  const passwordRef = useRef("");
   return (
     <React.Fragment>
       <h1>Crud</h1>
       <hr />
       <h2>Login</h2>
-      <input
-        type="text"
-        placeholder="email"
-        defaultValue={"j@gmail.com"}
-        ref={emailRef}
-      />
-      <input
-        type="text"
-        placeholder="password"
-        defaultValue={"jasur"}
-        ref={pwRef}
-      />
-      <button onClick={onLogin}>login</button>
-      <button onClick={() => localStorage.clear()}>log out</button>
+      <div>
+        <span>
+          email{" "}
+          <input ref={emailRef} defaultValue={"j@gmail.com"} type="text" />
+        </span>
+        <span>
+          passoword{" "}
+          <input ref={passwordRef} defaultValue={"jasur"} type="password" />
+        </span>
+        <button onClick={getUsers}>login</button>
+        <button onClick={() => localStorage.clear()}>logout</button>
+      </div>
     </React.Fragment>
   );
 }
--- src/lesson19/Read.jsx
+++ src/lesson19/Read.jsx
@@ -3,22 +3,19 @@ import React, { useState } from "react";
 export default function Read() {
   const [house, setHouse] = useState({});
   const getData = () => {
-    fetch("http://localhost:8080/api/v1/categories/list")
+    fetch("http://localhost:8080/api/v1/houses/me", {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
       .then((res) => res.json())
-      .then((res) => setHouse(res));
+      .then((res) => console.log(res));
   };
 
   return (
     <React.Fragment>
       <h2>Read</h2>
       <button onClick={getData}>get data</button>
-      {house?.data?.map(({ id, name }) => {
-        return (
-          <h3>
-            {id} - {name}
-          </h3>
-        );
-      })}
     </React.Fragment>
   );
 }
