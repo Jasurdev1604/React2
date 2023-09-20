8afbd740e87391fd41b85522abdbd00a5c73d5a5
Jasurbek <jasurdev1604@gmail.com>
2023-09-20 18:01:05 +0500

    axios

--- src/lesson20/index.jsx
+++ src/lesson20/index.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
 
 export default function index() {
+  useEffect(() => {
+    axios({
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: { name: "jaurbek" },
+      timeout: 10000,
+      url: "https://jsonplaceholder.typicode.com/users",
+    }).then((res) => console.log(res.data));
+  }, []);
+
   return (
     <React.Fragment>
-      <h1>index</h1>
+      <h1>Axios</h1>
     </React.Fragment>
   );
 }
--- src/lesson20/review.js
+++ src/lesson20/review.js
@@ -5,4 +5,49 @@
 // 	- manually vs automatic => data transform
 // 	- npm- install whatwg-fetch --save polyfill for-fetch
 
+import axios from "axios";
+
 //!Axios => thrid part library , auto transformer
+
+axios.post("endPoint", {
+  headers: {},
+  body: {},
+});
+
+//!axiosni import qipsh kerak react ga
+
+axios({
+  method: "GET",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: { name: "jaurbek" },
+  timeout: 10000,
+  url: "https://jsonplaceholder.typicode.com/users",
+}).then((res) => console.log(res.data));
+
+//timeout => qanchadir vaqtdan keyin requestni tioxtatadi
+
+//!fetchda timeout orniga AbordController ishlariladi
+
+const signal = new AbortController();
+
+let promise = fetch("https://", {
+  body: {},
+  headers: {},
+  signal,
+});
+
+setTimeout(() => signal.abort(), 1000);
+
+//!interceptors => rquest va response dan oldin nimadir bajarish uchun
+
+axios.interceptors.request.use((request) => {
+  console.log(request);
+  request.headers.Authorization = `Bearer token`;
+  return request;
+});
+axios.interceptors.response.use((response) => {
+  console.log(response);
+  return response;
+});
