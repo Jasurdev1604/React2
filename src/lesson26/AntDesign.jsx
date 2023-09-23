fc1ac0a6916436d3ccedd661ce72f90fc30635ba
Jasurbek <jasurdev1604@gmail.com>
2023-09-23 14:59:17 +0500

    AntDesign , Material UI

--- src/lesson26/AntDesign.jsx
+++ src/lesson26/AntDesign.jsx
@@ -1,23 +1,10 @@
 import React from "react";
-// import { Button, Space } from "antd";
 
 export default function AntDesign() {
   return (
     <React.Fragment>
       <div style={{ display: "flex", flexDirection: "column", flex: 1 }}>
         <h1>AntDesign</h1>
-        {/* <Space className="site-button-ghost-wrapper" wrap>
-          <Button type="primary" ghost>
-            Primary
-          </Button>
-          <Button ghost>Default</Button>
-          <Button type="dashed" ghost>
-            Dashed
-          </Button>
-          <Button type="primary" danger ghost>
-            Danger
-          </Button>
-        </Space> */}
       </div>
     </React.Fragment>
   );
--- src/lesson26/review.js
+++ src/lesson26/review.js
@@ -3,3 +3,7 @@
 // 	- Material UI(MUI)
 
 //!Antdesign and material ui -> compnent based librarys
+// yarn add antd
+
+//!material ui
+// yarn add @mui/material @mui/styled-engine-sc styled-components -> with styled componets
